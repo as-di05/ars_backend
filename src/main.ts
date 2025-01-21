892c82e5028649f67a2f8bc8fc2e6f7e451ca527
as-intelogos <adilet.ulukbekov@intelogos.com>
2025-01-22 00:12:16 +0600

    Second deploy

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(express.static(join(__dirname, '..', 'public')));
+  // app.use(express.static(join(__dirname, '..', 'public')));
 
-  app.use('*', (req, res) => {
-    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  });
+  // app.use('*', (req, res) => {
+  //   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  // });
 
   app.enableCors({
     // origin: 'http://localhost:3000',
