506d3f2cb1f3768b9ef5783f7949cc34879f9732
as-intelogos <adilet.ulukbekov@intelogos.com>
2025-01-22 00:10:20 +0600

    Second deploy

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(express.static(join(__dirname, '..', 'public')));
 
-  // app.use('*', (req, res) => {
-  //   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  // });
+  app.use('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
 
   app.enableCors({
     // origin: 'http://localhost:3000',
     origin: process.env.BASE_URL || 'https://turan-nedvijimost-27595b75bbe9.herokuapp.com',
   });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
