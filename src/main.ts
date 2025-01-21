1d93b6368cb9c9ed19112d42a0a8a77ac83c305e
as-intelogos <adilet.ulukbekov@intelogos.com>
2025-01-22 00:42:28 +0600

    Second deploy

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,31 @@ import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
 
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { join } from 'path';
+// import * as express from 'express';
+
+// async function bootstrap() {
+
+//   // Перенаправление всех остальных маршрутов на index.html
+//   app.use('*', (req, res) => {
+//     res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+//   });
+
+//   await app.listen(process.env.PORT || 3001, '0.0.0.0');
+//   console.log(`Server is running on: ${await app.getUrl()}`);
+// }
+// bootstrap();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.static(join(__dirname, '..', 'public')));
 
-  // app.use('*', (req, res) => {
-  //   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  // });
-
+  app.use('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
+  
   app.enableCors({
     // origin: 'http://localhost:3000',
     origin:
