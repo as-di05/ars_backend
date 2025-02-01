e0aa79af8381f25f8191798e197956f75e09fbb3
as-intelogos <adilet.ulukbekov@intelogos.com>
2025-02-01 22:40:28 +0600

    users api changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secretKey',
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '10d' },
     }),
   ],
   controllers: [AuthController],
