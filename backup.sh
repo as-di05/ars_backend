2fc145b577cd67cc052cc2fb9aa8e53d0d0df2b6
as-intelogos <adilet.ulukbekov@intelogos.com>
2025-02-15 18:56:53 +0600

    Create script for auto backup bd

--- backup.sh
+++ backup.sh
@@ -9,19 +9,11 @@ PASSWORD="d5ue2y4w7z8rabho"
 DATABASE="kn1av5tf8p91tiix"
 S3_BUCKET="turan-nedvijimost"
 
-# Убедитесь, что mysqldump доступен
-echo "Проверка наличия mysqldump..."
-which mysqldump || { echo "mysqldump не найден, устанавливаем..."; apt-get update && apt-get install -y mysql-client; }
-
-# Убедитесь, что aws-cli доступен
-echo "Проверка наличия aws-cli..."
-which aws || { echo "aws-cli не найден, устанавливаем..."; apt-get update && apt-get install -y awscli; }
-
 # Создание дампа базы данных
 echo "Создаём бэкап базы данных..."
 mysqldump -h "$HOST" -u "$USER" --password="$PASSWORD" "$DATABASE" > "$BACKUP_FILE"
 
-# Проверка успешности дампа
+# Проверяем успешность дампа
 if [ $? -ne 0 ]; then
   echo "Ошибка: не удалось создать бэкап!"
   exit 1
@@ -33,14 +25,10 @@ echo "Бэкап успешно создан: $BACKUP_FILE"
 echo "Загружаем бэкап в AWS S3..."
 aws s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/backups/"
 
-# Проверка успешности загрузки в S3
+# Проверяем успешность загрузки в S3
 if [ $? -ne 0 ]; then
   echo "Ошибка: не удалось загрузить бэкап в S3!"
   exit 1
 fi
 
 echo "Бэкап успешно загружен в S3."
-
-# Очистка временного файла
-echo "Удаляем локальный бэкап файл..."
-rm "$BACKUP_FILE"
