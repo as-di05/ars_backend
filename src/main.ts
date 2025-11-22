b99907a70bfab2eda791821c823400108f596072
as-di05 <as_di00@list.ru>
2025-11-22 14:42:05 +0600

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,21 @@ import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // === 1. STATIC FILES ===
   app.use(express.static(join(__dirname, '..', 'public')));
 
-  // === 2. WWW REDIRECT ===
+  // app.use('*', (req, res) => {
+  //   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  // });
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+    next();
+  });
+
   app.use((req, res, next) => {
     if (req.hostname === 'www.turan-nedvijimost.kg') {
       return res.redirect(301, `https://turan-nedvijimost.kg${req.url}`);
@@ -20,13 +26,10 @@ async function bootstrap() {
     next();
   });
 
-  // === 3. CORS ===
   app.enableCors({
-    origin: 'https://turan-nedvijimost.kg',
-    credentials: true,
+    // origin: 'http://localhost:3000',
+    origin: process.env.BASE_URL || 'https://turan-nedvijimost.kg',
   });
-
-  // === 4. GLOBAL SETTINGS ===
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -35,17 +38,9 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalInterceptors(new UnauthorizedInterceptor());
-
-  // === 5. CATCH-ALL HANDLER FOR SPA (React/Vite/Next export) ===
-  app.use('*', (req, res) => {
-    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  });
-
-  // === 6. START SERVER ===
   await app.listen(process.env.PORT || 3001, '0.0.0.0');
 
-  console.log(`Server is running at: ${await app.getUrl()}`);
+  const url = await app.getUrl();
+  console.log(`Server is running at: ${url}`);
 }
-
 bootstrap();
